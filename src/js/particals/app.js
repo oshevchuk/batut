d256967efc3f86fc9a9877f2105e07c2083cde4a
oshevchuk <megadevice83@gmail.com>
2018-03-21 20:22:10 +0200

    test'

--- src/js/particals/app.js
+++ src/js/particals/app.js
@@ -1,4 +1,4 @@
 /**
  * Created by Oshevchuk on 13.02.2018.
  */
-console.log(1); 
+// console.log(1);
--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,3 @@
  * Created by Oshevchuk on 12.02.2018.
  */
 
-//= ../../bower_components/jquery/dist/jquery.js
-
-//= particals/app.js
